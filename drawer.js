20a474d3bb1685a98de07ec019a54f4102cca8b9
9SDNANDSA89UND <idontuseskripts@gmail.com>
2025-11-21 13:28:19 +0000

    Update drawer.js

--- drawer.js
+++ drawer.js
@@ -3,17 +3,20 @@ const cartDrawer = document.getElementById("cartDrawer");
 const cartOverlay = document.getElementById("cartOverlay");
 const closeDrawer = document.getElementById("closeDrawer");
 
+/* OPEN DRAWER */
 cartBtn.addEventListener("click", () => {
   cartDrawer.classList.add("open");
   cartOverlay.classList.add("show");
-  updateCartDrawer(); // refresh contents
+  updateCartDrawer(); // refresh cart items on open
 });
 
+/* CLOSE DRAWER (X BUTTON) */
 closeDrawer.addEventListener("click", () => {
   cartDrawer.classList.remove("open");
   cartOverlay.classList.remove("show");
 });
 
+/* CLOSE DRAWER BY CLICKING OVERLAY */
 cartOverlay.addEventListener("click", () => {
   cartDrawer.classList.remove("open");
   cartOverlay.classList.remove("show");
